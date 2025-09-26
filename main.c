6dda0def4e5cd257f1a0782e5813d63466a7f66d
xrip <xrip@xrip.ru>
2025-09-26 19:29:56 +0300

    Fix MC6845 clock frequency calculation and remove outdated PLD descriptions
    - Corrected `MC6845_CLOCK_FREQ` definition by removing redundant trailing character.
    - Removed legacy PLD descriptions from `README.md

--- main.c
+++ main.c
@@ -31,7 +31,7 @@
 #define SYSTEM_CLOCK_HZ       (400 * MHZ)  // 400 MHz RP2040 core
 //#define MC6845_CLOCK_FREQ     (1789772.2f)      // ~NTSC master clock / 10
 #define BASE_CLOCK_FREQ (14.31818 * MHZ)
-#define MC6845_CLOCK_FREQ     ((BASE_CLOCK_FREQ) / 8)      /
+#define MC6845_CLOCK_FREQ     ((BASE_CLOCK_FREQ) / 8)
 
 // ---------------- Default register values ----------------
 static const uint8_t mc6845_defaults[16] = {
