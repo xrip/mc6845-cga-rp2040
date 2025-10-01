6ec13469994aeeeed32e4ec353babe473f469d65
xrip <xrip@xrip.ru>
2025-10-01 10:52:25 +0300

    Refactor `pld/decody.py` for improved flexibility in address width formatting
    - Updated `to_bin` function to accept variable width for binary formatting.
    - Modified `walk_map` to use dynamic width when printing memory and I/O maps.
    - Adjusted memory map and I/O map entries for more descriptive labels and consistent output.
    - Improved loop logic for identifying set bits.

--- pld/decody.py
+++ pld/decody.py
@@ -1,15 +1,15 @@
-def to_bin(h):
-    return format(h, '032b')
+def to_bin(h, width):
+    return format(h, width)
 
-def walk_map(m):
+def walk_map(m, width):
     for (start_offset, identifier) in sorted(m, key=lambda p: p[0]):
-        print(to_bin(start_offset) + ' ' + identifier)
+        print(to_bin(start_offset, width) + ' ' + identifier)
 
         # identify set bits
         set_bits = []
 
         address = 0
-        while address <= 31:
+        while address <= 20:
             if(start_offset) & 1 != 0:
                 set_bits.append(address)
             address += 1 # increment address line
@@ -19,12 +19,10 @@ def walk_map(m):
         print(' bits set:', ', '.join(formatted_addresses))
 
 memory_map = [
-    ( 8191, 'CGA RAM' ),
-    ( 8192, 'CGA RAM' ),
-    ( 0xB8000, 'CGA RAM' ),
-    ( 0xB9000, 'CGA RAM' ),
-    ( 0xBA000, 'CGA RAM' ),
-    ( 0xBB000, 'CGA RAM' ),
+    ( 0xB8000, 'CGA RAM 0xB8000' ),
+    ( 0xB9000, 'CGA RAM 0xB9000' ),
+    ( 0xBA000, 'CGA RAM 0xBA000' ),
+    ( 0xBB000, 'CGA RAM 0xBB000' ),
 ]
 """
 memory_map = [ # PV7
@@ -37,14 +35,16 @@ memory_map = [ # PV7
 """
 
 io_map = [
-    ( 0x3D8, 'IO 3D8h' ),
-    ( 0x3D9, 'IO 3D9h' ),
-    ( 0x3DA, 'IO 3DAh' ),
+    ( 0x3D4, 'CRTC 3D4h' ),
+    ( 0x3D5, 'CRTC 3D5h' ),
+    ( 0x3D8, 'CGA 3D8h' ),
+    ( 0x3D9, 'CGA 3D9h' ),
+    ( 0x3DA, 'CGA 3DAh' ),
 
 ]
 
-print('Memory Map:')
-walk_map(memory_map)
+print('Memory Map 20b:')
+walk_map(memory_map, '020b')
 
-print('I/O Map:')
-walk_map(io_map)
+print('I/O Map 10b:')
+walk_map(io_map, '010b')
